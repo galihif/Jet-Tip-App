a2dbec5e452be5be4d8d1ed5b0a637b3acb82124
Galih Indra Firmansyah <galihindra2401@gmail.com>
2022-09-19 19:35:09 +0700

    ui: create TopHeader content

--- app/src/main/java/com/giftech/jettipapp/MainActivity.kt
+++ app/src/main/java/com/giftech/jettipapp/MainActivity.kt
@@ -3,18 +3,17 @@ package com.giftech.jettipapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.material.primarySurface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.giftech.jettipapp.ui.theme.JetTipAppTheme
@@ -44,18 +43,28 @@ fun MyApp(content: @Composable () -> Unit) {
 
 @Preview
 @Composable
-fun TopHeader() {
+fun TopHeader(totalPerPerson: Double = 0.0) {
+    val total = "%.2f".format(totalPerPerson)
     Surface(
         modifier = Modifier
             .fillMaxWidth()
             .height(150.dp)
-            .clip(
-                shape = RoundedCornerShape(16.dp),
-            ),
-        color = MaterialTheme.colors.primarySurface
+            .clip(shape = RoundedCornerShape(16.dp)),
+        color = Color(0xFFE9D7F7)
     ) {
-        Column {
-
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                "Total Per Person",
+                style = MaterialTheme.typography.h5
+            )
+            Text(
+                "$$total",
+                style = MaterialTheme.typography.h4,
+                fontWeight = FontWeight.Bold
+            )
         }
     }
 }
